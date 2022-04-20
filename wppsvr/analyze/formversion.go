75788baa3f86feb7e7d0ee7af03d8f87dae49ef0
Steve Roth <steve@rothskeller.net>
2022-04-20 16:31:42 -0700

    Don't accept "undefined" as a form version.

--- wppsvr/analyze/formversion.go
+++ wppsvr/analyze/formversion.go
@@ -28,7 +28,7 @@ func (a *Analysis) checkFormVersion() {
 		return
 	}
 	minimums := config.Get().MinimumVersions
-	if older(form.PIFOVersion, minimums["PackItForms"]) {
+	if older, valid := older(form.PIFOVersion, minimums["PackItForms"]); older || !valid {
 		a.problems = append(a.problems, &problem{
 			code:    ProblemPIFOVersion,
 			subject: "PackItForms version out of date",
@@ -39,48 +39,55 @@ messages containing forms.
 `, form.PIFOVersion, minimums[config.PackItForms]),
 		})
 	}
-	if minimums[form.FormName] != "" && older(form.FormVersion, minimums[form.FormName]) {
-		a.problems = append(a.problems, &problem{
-			code:    ProblemFormVersion,
-			subject: "form version out of date",
-			response: fmt.Sprintf(`
+	if minimums[form.FormName] != "" {
+		if older, valid := older(form.FormVersion, minimums[form.FormName]); older || !valid {
+			a.problems = append(a.problems, &problem{
+				code:    ProblemFormVersion,
+				subject: "form version out of date",
+				response: fmt.Sprintf(`
 This message contains version %s of the %s, but that version is not current.
 Please use version %s or newer of the form.  (You can get the newer form by
 updating your PackItForms installation.)
 `, form.FormVersion, form.TypeName(), minimums[form.FormName]),
-		})
+			})
+		}
 	}
 }
 
 // older compares two version numbers.  Version numbers are a dot-separated
 // sequence of parts, each of which is compared independently.  The parts are
 // compared numerically if they parse as integers, and as strings otherwise.
-func older(a, b string) bool {
+// The valid flag is set if any component of the first version number is a valid
+// integer.
+func older(a, b string) (older, valid bool) {
 	aparts := strings.Split(a, ".")
 	bparts := strings.Split(b, ".")
 	for len(aparts) != 0 && len(bparts) != 0 {
 		anum, aerr := strconv.Atoi(aparts[0])
+		if aerr == nil {
+			valid = true
+		}
 		bnum, berr := strconv.Atoi(bparts[0])
 		if aerr == nil && berr == nil {
 			if anum < bnum {
-				return true
+				return true, valid
 			}
 			if anum > bnum {
-				return false
+				return false, valid
 			}
 		} else {
 			if aparts[0] < bparts[0] {
-				return true
+				return true, valid
 			}
 			if aparts[0] > bparts[0] {
-				return false
+				return false, valid
 			}
 		}
 		aparts = aparts[1:]
 		bparts = bparts[1:]
 	}
 	if len(bparts) != 0 {
-		return true
+		return true, valid
 	}
-	return false
+	return false, valid
 }
