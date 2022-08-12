fdd5f31b96490311aeaa44274ca0a3a1cb1497c9
Steve Roth <steve@rothskeller.net>
2022-08-12 09:58:54 -0700

    Warn on ill-formatted date in subject.

--- wppsvr/analyze/practice.go
+++ wppsvr/analyze/practice.go
@@ -23,44 +23,12 @@ func init() {
 // practiceRE matches a correctly formatted practice subject.  The subject must
 // have the word Practice followed by four comma-separated fields (with
 // whitespace also allowed between the fields).  The RE returns the first field
-// (the call sign) and the fourth field (the date) as substrings so that they
-// can be further checked.
+// (the call sign), the third field (the jurisdiction), and the fourth field
+// (the date) as substrings so that they can be further checked and stored.
 // A comma is allowed after the word "Practice", which doesn't exactly conform
 // to the required syntax, but it is a very common mistake and not worth
 // penalizing.
-var practiceRE = regexp.MustCompile(`(?i)^Practice[,\s]+([AKNW][A-Z]?[0-9][A-Z]{1,3})\s*,[^,]+,([^,]+),\s*([^,]+?)\s*$`)
-
-// dateREs is a map from date format string to regular expression.  There is no
-// standard for the formatting of the date on the subject line, but we try all
-// of the common ones.
-var dateREs = map[string]*regexp.Regexp{
-	// These are all patterns seen in actual check-in data.  The point of
-	// parsing this is to verify that the date correctly identifies the
-	// check-in period, not to verify the syntax of the date, so we accept
-	// as many patterns as we can reasonably parse.
-	"1-2-2006":        regexp.MustCompile(`^\d?\d-\d?\d-20\d\d$`),
-	"1/2/2006":        regexp.MustCompile(`^\d?\d/\d?\d/20\d\d$`),
-	"2 Jan 2006":      regexp.MustCompile(`(?i)^\d?\d (?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec) 20\d\d$`),
-	"2-Jan-2006":      regexp.MustCompile(`(?i)^\d?\d-(?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)-20\d\d$`),
-	"2006-1-2":        regexp.MustCompile(`^20\d\d-\d?\d-\d?\d$`),
-	"2006-Jan-2":      regexp.MustCompile(`(?i)^20\d\d-(?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)-\d?\d$`),
-	"2006/1/2":        regexp.MustCompile(`^20\d\d/\d?\d/\d?\d$`),
-	"20060102":        regexp.MustCompile(`^20\d\d\d\d\d\d$`),
-	"2Jan2006":        regexp.MustCompile(`(?i)^\d?\d(?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)20\d\d$`),
-	"Jan 2, 2006":     regexp.MustCompile(`(?i)^(?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec) \d?\d, 20\d\d$`),
-	"Jan-2-2006":      regexp.MustCompile(`(?i)^(?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)-\d?\d-20\d\d$`),
-	"Jan. 2, 2006":    regexp.MustCompile(`(?i)^(?:jan|feb|mar|apr|jun|jul|aug|sep|oct|nov|dec)\. \d?\d, 20\d\d$`),
-	"January 2, 2006": regexp.MustCompile(`(?i)^(?:january|february|march|april|may|june|july|august|september|october|november|december) \d?\d, 20\d\d$`),
-	// Repeat all of the above with two-digit years, except where that
-	// doesn't make sense.
-	"1-2-06":   regexp.MustCompile(`^\d?\d-\d?\d-\d\d$`),
-	"1/2/06":   regexp.MustCompile(`^\d?\d/\d?\d/\d\d$`),
-	"2 Jan 06": regexp.MustCompile(`(?i)^\d?\d (?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec) \d\d$`),
-	"2-Jan-06": regexp.MustCompile(`(?i)^\d?\d-(?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)-\d\d$`),
-	"060102":   regexp.MustCompile(`^\d\d\d\d\d\d$`),
-	"2Jan06":   regexp.MustCompile(`(?i)^\d?\d(?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)\d\d$`),
-	"Jan-2-06": regexp.MustCompile(`(?i)^(?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)-\d?\d-\d\d$`),
-}
+var practiceRE = regexp.MustCompile(`(?i)^Practice[,\s]+([AKNW][A-Z]?[0-9][A-Z]{1,3})\s*,[^,]+,([^,]+),\s*((?:0?[1-9]|1[0-2])/(?:0?[1-9]|[12]\d|3[01])/20\d\d)\s*$`)
 
 // ProbPracticeSubjectFormat is raised when the practice message details in the
 // subject line have the wrong format.
@@ -102,15 +70,7 @@ var ProbPracticeSubjectFormat = &Problem{
 		if abbr, ok := jurisdictionMap[strings.ToUpper(a.jurisdiction)]; ok {
 			a.jurisdiction = abbr
 		}
-		// Look for a date.
-		for fmt, re := range dateREs {
-			if re.MatchString(match[3]) {
-				if date, err := time.ParseInLocation(fmt, match[3], time.Local); err == nil {
-					a.subjectDate = date
-					break
-				}
-			}
-		}
+		a.subjectDate, _ = time.ParseInLocation("1/2/2006", match[3], time.Local)
 		return false, ""
 	},
 	Variables: variableMap{
