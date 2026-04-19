9377f5010d67193d4cf61510271f151390a66067
Steve Roth <steve@rothskeller.net>
2026-04-19 07:55:26 -0700

    Bug fix for inclusion of definitions.html.

--- forms/allforms.go
+++ forms/allforms.go
@@ -4,5 +4,12 @@ package forms
 
 import "embed"
 
-//go:embed SCCoPIFO
+//go:embed SCCoPIFO/*/*.form
+//go:embed SCCoPIFO/*/*.html
+//go:embed SCCoPIFO/*/*.pdf
+//go:embed SCCoPIFO/definitions.html
+//go:embed SCCoPIFO/pack-it-forms.*
+//go:embed SCCoPIFO/README.txt
+//go:embed SCCoPIFO/SCCoPIFO.*
+//go:embed SCCoPIFO/update.json
 var EmbeddedForms embed.FS
--- forms/sccopifo.go
+++ forms/sccopifo.go
@@ -4,5 +4,15 @@ package forms
 
 import "embed"
 
-//go:embed SCCoPIFO
+// Unfortunately we have to list all of the embeds separately, because the
+// SCCoPIFO repository also contains Word documents, and we don't want those.
+
+//go:embed SCCoPIFO/*/*.form
+//go:embed SCCoPIFO/*/*.html
+//go:embed SCCoPIFO/*/*.pdf
+//go:embed SCCoPIFO/definitions.html
+//go:embed SCCoPIFO/pack-it-forms.*
+//go:embed SCCoPIFO/README.txt
+//go:embed SCCoPIFO/SCCoPIFO.*
+//go:embed SCCoPIFO/update.json
 var EmbeddedForms embed.FS
