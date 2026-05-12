74d4d0737d2db0e88f878f9768495fe1aaf99e9a
Steve Roth <steve@rothskeller.net>
2026-05-12 09:50:19 -0700

    Bug fix: from address in log entry for receipt

--- incident/receive.go
+++ incident/receive.go
@@ -152,6 +152,8 @@ func (i *Incident) receiveReceiptMessage(rcpt *message.JustReceivedMessage) (err
 		Time:    rcpt.RxDate(),
 		Subject: rcpt.Subject().EncodedSubject(),
 	}
+	rcptle.FromCall, _, _ = strings.Cut(rcpt.From(), "@")
+	rcptle.FromCall = strings.ToUpper(strings.TrimSpace(rcptle.FromCall))
 	var smr = message.SentMessageReceipt{ReceiverAddress: rcpt.From()}
 	var subject string
 	if rcpt.MType == receipt.DeliveryReceipt {
