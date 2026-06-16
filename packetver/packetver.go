faa136b7699c2a04256c346238d768377ee8ba74
Steve Roth <steve@rothskeller.net>
2026-06-16 12:55:22 -0700

    Update version number to 4.0.14

--- packetver/packetver.go
+++ packetver/packetver.go
@@ -3,7 +3,7 @@ package packetver
 // This is the software version number; it should be updated at every published
 // software change.  Note this is distinct from pifover.PIFOVerson, which is the
 // form engine version number.
-const Version = "4.0.13"
+const Version = "4.0.14"
 
 // When changing this, don't forget to change the references in
 // cmd/packet/winres.json as well.
